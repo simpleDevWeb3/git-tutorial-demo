a875cba2fb6b387933071d4fc4d2c4b101cc3de3
ChengST02 <chengst-pm24@student.tarc.edu.my>
2025-07-12 19:56:09 +0800

    HTMl tag is added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log("Updated Version 12");
-<p>this message wrote by lee</p>
-<p>this is the message wrote by Cheng</p>
+
+<html>
+    <p>this message wrote by lee</p>
+    <p>this is the message wrote by Cheng</p>
+</html>
